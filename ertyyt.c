5696331ae4f3060c54a93f7bd0c84a65b9859b35
Saif Islam Rayhan <134764307+rayhanosd@users.noreply.github.com>
2023-05-29 13:16:51 +0600

    if else

---
+++ ertyyt.c
@@ -0,0 +1,16 @@
+    #include<stdio.h>
+    int main ()
+    {
+        int n;
+        n = 10;
+        if (n<10) {
+            printf ("The number of negative\n");
+
+
+        }
+        else {
+            printf ("The number of positive\n");
+        }
+
+        return 0;
+    }
